9b8af425703cb08f88e50a4d016f3bf0de7f5237
Tiago <tiagocustodio@alunos.utfpr.edu.br>
2024-10-16 14:02:28 -0300

    Add files via upload

---
+++ bancodigital.rb
@@ -0,0 +1,32 @@
+contasCorrentes = { 12345-6 => 87144798, 23456-7 => 92027944}
+
+loop do
+
+    puts "Bem vindo ao seu Banco Digital!"
+    puts "Escolha uma opção 1 - Sou cliente 2 - Quero ser cliente 3 - Sair"
+    opcaoMenu = gets.chomp.to_i
+
+    case opcaoMenu 
+    when 1
+        puts "Informe o número da sua conta-corrente (formato xxxxx-x)"
+        numeroConta = gets.chomp.strip
+
+        puts "Informe a sua senha:"
+        senha = gets.chomp 
+
+        regex = /^\d{5}-\d{1}$/
+    
+        if numeroConta.match?(regex)
+            puts "Conta correta!"
+        else
+            puts "Formato inválido!"
+        end
+    
+    when 2
+        puts "Vamos fazer o seu cadastro..."
+    when 3
+        puts "Saindo..."
+        break
+    end
+
+end
